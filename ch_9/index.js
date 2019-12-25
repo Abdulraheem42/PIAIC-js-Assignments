f55313340eb5d6324179d83063772600acc868fe
Abdulraheem42 <abdulraheem4255395@gmail.com>
2019-12-25 12:32:31 +0500

    chp: 09 assignment completed

---
+++ ch_9/index.js
@@ -0,0 +1,5 @@
+var firstName = prompt("Enter your First Name");
+var lastName = prompt("Enter your Last Name");
+var age = prompt("Enter your age");
+
+alert("Hello" + " " + firstName + " " + lastName + "," + " " + "you are" + " " + age + " " + "years old");
