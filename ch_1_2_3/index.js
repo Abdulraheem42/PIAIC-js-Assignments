c608e3c8a7afe5ed82b707780bd67748c535a863
Abdulraheem42 <abdulraheem4255395@gmail.com>
2019-12-25 11:25:52 +0500

    chp: 1, 2 & 3 assignment completed

---
+++ ch_1_2_3/index.js
@@ -0,0 +1,5 @@
+var firstName = "Abdul";
+var lastName = "Raheem";
+var age = 22;
+
+alert("Hello" + " " + firstName + " " + lastName + "," + " " + "You are" + " " + age + " " +"years" + " " + "old");
